5efd47e225e7883b3bad828ce88e7a8a6fa8ed29
Arthur Nunes Catarina <arthurn.catarina@gmail.com>
2020-05-05 21:32:41 -0300

    Adicionado array para historico

--- Projetos-Backend/calculadora/src/main/java/br/edu/usj/calculadora/Calculadora.java
+++ Projetos-Backend/calculadora/src/main/java/br/edu/usj/calculadora/Calculadora.java
@@ -1,5 +1,8 @@
 package br.edu.usj.calculadora;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -8,34 +11,48 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class Calculadora {
 
+    List<String> listaOperacoes = new ArrayList<>();
+
     @PostMapping(value = "/calcula")
     public ModelAndView postCalcula(@RequestParam String num1, @RequestParam String num2,
             @RequestParam String operador) {
 
+            
+
         ModelAndView modelAndView = new ModelAndView("index");
         Double resultado = 0.0;
         String texto;
+        String operacao = num1 + operador + num2 +"=" + resultado;
         switch (operador) {
             case "+":
                 resultado = Double.parseDouble(num1) + Double.parseDouble(num2);
                 texto = "O resultado é " + resultado;
+                listaOperacoes.add(operacao);
                 modelAndView.addObject("mensagem", texto);
+                modelAndView.addObject("historico", listaOperacoes);
                 break;
             case "-":
                 resultado = Double.parseDouble(num1) - Double.parseDouble(num2);
                 texto = "O resultado é " + resultado;
+                listaOperacoes.add(operacao);
                 modelAndView.addObject("mensagem", texto);
+                modelAndView.addObject("historico", listaOperacoes);
                 break;
             case "x":
                 resultado = Double.parseDouble(num1) * Double.parseDouble(num2);
                 texto = "O resultado é " + resultado;
+                listaOperacoes.add(operacao);
                 modelAndView.addObject("mensagem", texto);
+                modelAndView.addObject("historico", listaOperacoes);
                 break;
             case "/":
                 resultado = Double.parseDouble(num1) / Double.parseDouble(num2);
                 texto = "O resultado é " + resultado;
+                listaOperacoes.add(operacao);
                 modelAndView.addObject("mensagem", texto);
+                modelAndView.addObject("historico", listaOperacoes);
                 break;
+                
         }
 
         
