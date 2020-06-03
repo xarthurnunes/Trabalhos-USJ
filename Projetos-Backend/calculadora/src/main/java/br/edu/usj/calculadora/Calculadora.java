c790ea78b0cbb794cda1b04d02dcf5cf256edef7
Arthur Nunes Catarina <arthurn.catarina@gmail.com>
2020-06-02 21:18:45 -0300

    Estilizado com bootstrap

--- Projetos-Backend/calculadora/src/main/java/br/edu/usj/calculadora/Calculadora.java
+++ Projetos-Backend/calculadora/src/main/java/br/edu/usj/calculadora/Calculadora.java
@@ -2,6 +2,8 @@ package br.edu.usj.calculadora;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
@@ -52,6 +54,26 @@ public class Calculadora {
         modelAndView.addObject("mensagem", texto);
         modelAndView.addObject("historico", operacaoRepository.findAll());
 
+        return modelAndView;
+    }
+
+    @GetMapping(value="/deleta/{id}")
+    public ModelAndView getDeleta(@PathVariable Long id) {
+        operacaoRepository.deleteById(id);
+        ModelAndView modelAndView = new ModelAndView("index");
+        modelAndView.addObject("historico", operacaoRepository.findAll());
+        return modelAndView;
+    }
+    @GetMapping(value="/mostra/{id}")
+    public ModelAndView getMostra(@PathVariable Long id) {
+        String texto = "";
+        if (operacaoRepository.findById(id).isPresent()) {
+        Operacao operacao = operacaoRepository.findById(id).get();
+        texto = operacao.getId() + ": " + operacao.getOperacao();
+        }
+        
+        ModelAndView modelAndView = new ModelAndView("mostra-operacao");
+        modelAndView.addObject("texto", texto);
         return modelAndView;
     }
 
