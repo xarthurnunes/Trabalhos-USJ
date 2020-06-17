6e349caa078baec033a702928db8c0b8a2a917bd
Arthur Nunes Catarina <xarthur.nuness@gmail.com>
2020-06-16 22:16:16 -0300

    Criaçao do BackEnd

---
+++ Projetos-Backend/drinkstorebackend/src/main/java/br/edu/usj/drinkstorebackend/DrinkStoreBackEndApplication.java
@@ -0,0 +1,13 @@
+package br.edu.usj.drinkstorebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrinkStoreBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DrinkStoreBackEndApplication.class, args);
+	}
+
+}
