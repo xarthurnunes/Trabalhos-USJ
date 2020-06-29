8a4159c38586c9a531dedc82ecd1d6134f1dba55
Arthur Nunes Catarina <xarthur.nuness@gmail.com>
2020-06-29 16:02:26 -0300

    Ajustes

--- sistemaxyz/src/main/java/br/edu/usj/sistemaxyz/LoginController.java
+++ sistemaxyz/src/main/java/br/edu/usj/sistemaxyz/LoginController.java
@@ -1,19 +1,20 @@
 package br.edu.usj.sistemaxyz;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController {
 
-    @GetMapping(value = "/login.html") 
+    @PostMapping(value = "/login") 
         public String getLogin(@RequestParam String usuario, @RequestParam String senha) {
-            if (usuario == "admin" && senha == "qwe123") {
-                return "redirect:/login.html";
-            } else {
-                return "redirect:/";	
-            }
+            return "login";
+        //    if (usuario == "admin" && senha == "qwe123") {
+        //        return "login";
+        //    } else {
+        //        return "index";	
+        //    }
             
     }
     
