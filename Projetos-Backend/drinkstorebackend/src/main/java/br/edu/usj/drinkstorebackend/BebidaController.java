ddfeeabac81a4e24b428f8d4356ae68b8f65e17f
Arthur Nunes Catarina <xarthur.nuness@gmail.com>
2020-07-03 21:35:07 -0300

    Criado metodos para input VueJS

--- Projetos-Backend/drinkstorebackend/src/main/java/br/edu/usj/drinkstorebackend/BebidaController.java
+++ Projetos-Backend/drinkstorebackend/src/main/java/br/edu/usj/drinkstorebackend/BebidaController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -38,7 +39,7 @@ public class BebidaController {
     }
 
     @PostMapping(value="/cadastrar")
-    public Bebida postCadastrar(Bebida bebida) {
+    public Bebida postCadastrar(@RequestBody Bebida bebida) {
         //recebe a bebida preenchida no formulário e grava no banco
         Bebida bebidaSalva = bebidaRepository.save(bebida);
         return bebidaSalva;
